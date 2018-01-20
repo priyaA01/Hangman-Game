b3b38ba13529c9092f9677aedc8d3915f136dfa2
priyaA01 <pryshanthi.it@gmail.com>
2018-01-19 19:57:45 -0600

    updated version 2

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,125 +1,96 @@
-var lettersAlreadyGuessed=[];
-	var wins=0;
-	var losses=0;
-	var guessesRemaining=0;
-	var lettersGuessed=[];
+/*  global variables declaration */
+var lettersAlreadyGuessed = [];
+var wins = 0;
+var losses = 0;
+var guessesRemaining = 0;
+var lettersGuessed = [];
+
+/* function  to start game ,displays the hidden word to find , and allows user to strt entering letters*/
+function wordList() {
+    document.getElementById("errorMsg").style.display = "none";
+    document.getElementById("word").innerHTML = "";
+    document.getElementById("wordHidden").innerHTML = "";
+    document.getElementById("letter").innerHTML = "";
+    document.getElementById("lettersGuessed").innerHTML = "";
+    document.getElementById("guessesRemaining").innerHTML = "10";
+    guessesRemaining = 10;
+    lettersGuessed = [];
+    var words = ["Australia", "Brazil", "Canada", "Denmark", "Egypt", "France", "India", "Japan", "China", "Mexico"];
+    var wordToFind = words[Math.floor(Math.random() * words.length)];
+    var displayHyphen = "";
+
+    for (var i = 0; i < wordToFind.length; i++) {
+
+        displayHyphen = displayHyphen + '-';
+    }
 
 
-function wordList(){
-	document.getElementById("errorMsg").style.display="none";
-	document.getElementById("word").innerHTML="";
-	document.getElementById("wordHidden").innerHTML="";
-	document.getElementById("letter").innerHTML="";
-	guessesRemaining=10;
-	var words=["Australia","Brazil","Canada","Denmark","Egypt","France","India","Japan","China","Mexico"];
-	var wordToFind=words[Math.floor(Math.random()*words.length)];
-
-	var displayHyphen="";
-
-	for(var i=0;i<wordToFind.length;i++)
-	{		
-
-		displayHyphen= displayHyphen +'-';
-	}
-	
-	document.getElementById("word").innerHTML=displayHyphen;
-	document.getElementById("wordHidden").innerHTML= wordToFind;
-	console.log("Word to find   " + wordToFind);
+    document.getElementById("word").innerHTML = displayHyphen;
+    document.getElementById("wordHidden").innerHTML = wordToFind;
+    console.log("Word to find   " + wordToFind);
 
 
-document.onkeyup = function(event){
-	var letterIndex="";
-    var x = event.keyCode || event.which;    
-    var y = String.fromCharCode(x); 
-    if(x>=65 && x<=90 || x >=97 && x<=122)
-    {          
-    	document.getElementById("errorMsg").style.display="none";
-	    document.getElementById("letter").innerHTML =  y.toLowerCase();
-	    matchingLetters(y.toLowerCase()); 
-    } 
- 	else
- 	{
-    	document.getElementById("errorMsg").innerHTML ="Please enter letters from a-z";
-    	document.getElementById("errorMsg").style.display="block";
-    } 
+    document.onkeyup = function(event) {
+
+        var x = event.keyCode || event.which;
+        var y = String.fromCharCode(x);
+        if (x >= 65 && x <= 90 || x >= 97 && x <= 122) {
+            document.getElementById("errorMsg").style.display = "none";
+            document.getElementById("letter").innerHTML = y.toLowerCase();
+            matchingLetters(y.toLowerCase());
+        } else {
+            document.getElementById("errorMsg").style.display = "block";
+            document.getElementById("errorMsg").innerHTML = "Please enter letters from a-z";
+        }
+    }
+
 }
-	
-	//return wordToFind;	
-}
 
+/* function to find user entered letters match with the word ,if it matches letters show up in the word*/
+function matchingLetters(letterEntered) {
+    var wordToFind = (document.getElementById("wordHidden").innerHTML).toLowerCase();
+    var displayHyphen = document.getElementById("word").innerHTML;
 
-function matchingLetters(letterEntered)
-{
-	var wordToFind=(document.getElementById("wordHidden").innerHTML).toLowerCase();
-	var displayHyphen=document.getElementById("word").innerHTML;
-	
-	console.log(wordToFind + "and" +  displayHyphen.length );
+    console.log(wordToFind + "and" + displayHyphen.length);
 
-		if(wordToFind.indexOf(letterEntered)>-1)
-		{
-			for(var i=0;i < wordToFind.length;i++)
-			{
-				if(wordToFind.charAt(i)==letterEntered)
-				{
-					displayHyphen=setCharAt(displayHyphen,i,letterEntered);		
-				}
-			}
-		}
-		else 
-		{
-			if(lettersGuessed.indexOf(letterEntered)===-1)
-			{
-			lettersGuessed.push(letterEntered);
-			}
-			document.getElementById("lettersGuessed").innerHTML = lettersGuessed;
+    if (wordToFind.indexOf(letterEntered) > -1) {
+        for (var i = 0; i < wordToFind.length; i++) {
+            if (wordToFind.charAt(i) == letterEntered) {
+                displayHyphen = setCharAt(displayHyphen, i, letterEntered);
+            }
+        }
+    } else {
+        document.getElementById("wrongSound").play();
+        if (lettersGuessed.indexOf(letterEntered) === -1) {
+            lettersGuessed.push(letterEntered);
+        }
+        document.getElementById("lettersGuessed").innerHTML = lettersGuessed;
 
-			guessesRemaining--;
-			document.getElementById("guessesRemaining").innerHTML = guessesRemaining;
-			console.log("letter is not in the word"  + guessesRemaining );
-		}
-		
-		document.getElementById("word").innerHTML=displayHyphen;
+        guessesRemaining--;
+        document.getElementById("guessesRemaining").innerHTML = guessesRemaining;
+        console.log("letter is not in the word" + guessesRemaining);
+    }
 
-		if(displayHyphen===wordToFind)
-		{
-			wins++;
-			document.getElementById("wins").innerHTML = wins;
-			console.log("score increased " +wins);
-			wordList();
-		}
-		else if( displayHyphen!=wordToFind && guessesRemaining===0)
-		{
-			losses++;
+    document.getElementById("word").innerHTML = displayHyphen;
+
+    if (displayHyphen === wordToFind) {
+        //mySound.play();
+        document.getElementById("correctSound").play();
+        wins++;
+        document.getElementById("wins").innerHTML = wins;
+        console.log("score increased " + wins);
+        wordList();
+    } else if (displayHyphen != wordToFind && guessesRemaining === 0) {
+        /*losses++;
 			document.getElementById("losses").innerHTML = losses;
-			console.log("You Lost ! Try again " +losses);
-			wordList();
-		}
-		//wins++;
-		//wordList();
+			console.log("You Lost ! Try again " +losses);*/
+        wordList();
+    }
+
 }
 
-
-function setCharAt(str,index,chr) {
-    if(index > str.length-1) return str;
-    return str.substr(0,index) + chr + str.substr(index+1);
+/*function to replace the letter matched in the word in that position*/
+function setCharAt(str, index, chr) {
+    if (index > str.length - 1) return str;
+    return str.substr(0, index) + chr + str.substr(index + 1);
 }
-
-/*
-
-/*var x = document.createElement("INPUT");
-		x.setAttribute("type", "text"); 
-		x.setAttribute("id",i);
-		x.setAttribute("size","1");  
-		document.getElementById("word").appendChild(x);                    
-		//document.body.appendChild(x);
-		//displayHyphen=displayHyphen.concat(x);
-			//"<input type='text' id="+i+" name='fname' size='1'>" );
-
-letterReplace(i,letterEntered);
-			    	/*console.log("letter is in the word  "  + letterEntered );
-			    	displayHyphen=displayHyphen.charAt(i).replace(letterEntered);*/
-			    	//console.log(displayHyphen);
-			    	/*document.getElementById("1").innerHTML=letterEntered;
-			    	console.log(document.getElementById("1").innerHTML);*/
-			    	//displayHyphen[i].replace(displayHyphen[i],letterEntered);
-			    	//displayHyphen.charAt(i).replace(letterEntered);	*/
